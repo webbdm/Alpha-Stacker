0189fba50fbda49c42afbc8cdd198da03840330b
webbdm <gwebb762@gmail.com>
2017-02-07 21:28:52 -0600

    first commit

---
+++ alpha-stacker.js
@@ -0,0 +1,13 @@
+// Create an array that contains the letters of the alphabet
+var alphabet = [];
+
+// The stackLetter function should accept the array as the sole argument
+function stackLetters (theAlphabetArray) {
+    /*
+      Write a `for` loop that iterates over the array argument and
+      outputs the letters.
+     */
+}
+
+// Invoke the function and pass in the array
+stackLetters(alphabet)
