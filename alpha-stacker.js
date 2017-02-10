99274556441f05a115f023ccb1a3894bdf791599
webbdm <gwebb762@gmail.com>
2017-02-09 20:48:29 -0600

    added from Codepen account

--- alpha-stacker.js
+++ alpha-stacker.js
@@ -1,12 +1,26 @@
-// Create an array that contains the letters of the alphabet
-var alphabet = [];
+// Create an array that contains the letters of the alphabet 
+var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+var newAlpha = [];
 
 // The stackLetter function should accept the array as the sole argument
-function stackLetters (theAlphabetArray) {
+function stackLetters (alphabet) {
     /*
       Write a `for` loop that iterates over the array argument and
       outputs the letters.
      */
+    for(i=0; i < alphabet.length; i++){
+  
+      newAlpha += alphabet[i];
+      console.log(newAlpha);
+      
+      if ([i+1] % 3 === 0){
+        
+          var blank = " ";
+          newAlpha += blank;
+        
+      } 
+   }
 }
 
 // Invoke the function and pass in the array
